1c3addb9e83d64d7938df8441a879c0da0f8e149
Wisanu Boonrat <widnusleepy@gmail.com>
2021-02-16 16:29:17 +1300

    remove configuration in app.run

--- sentiment_api.py
+++ sentiment_api.py
@@ -197,4 +197,4 @@ def api_chatbot():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run()
